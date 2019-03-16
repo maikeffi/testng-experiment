e1b0cdc0c1566d2a413f6a30a3a990b5fdd454f1
maikeffi <maikeffi@gmail.com>
2019-03-16 22:48:08 +0530

    first commit

---
+++ src/test/java/com/maikeffi/test/experiment/TestGroupExample.java
@@ -0,0 +1,29 @@
+package com.maikeffi.test.experiment;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+public class TestGroupExample {
+
+    @BeforeSuite
+    public void testBeforeSuite() {
+        System.out.println("testBeforeSuite()");
+    }
+
+    @AfterSuite
+    public void testAfterSuite() {
+        System.out.println("testAfterSuite()");
+    }
+
+    @BeforeTest
+    public void testBeforeTest() {
+        System.out.println("testBeforeTest()");
+    }
+
+    @AfterTest
+    public void testAfterTest() {
+        System.out.println("testAfterTest()");
+    }
+}
